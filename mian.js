fe3f4009b47feea3f0982a86d86357d0e29bcbbf
Shraiy01 <113162654+Shraiy01@users.noreply.github.com>
2023-09-06 16:33:51 +0530

    Add files via upload

---
+++ mian.js
@@ -0,0 +1,8 @@
+
+
+const item=document.querySelectorAll('.item');
+console.log(item);
+// item.innerHTML='<h1>HELLO</h1>';
+// item.style.color='green';
+// const list=document.querySelector('.items');
+// list.children[1].style.color='yellow';
