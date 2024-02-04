5223ca369f2f471f369e9b6763c7a76d0b757732
Shraiy01 <113162654+Shraiy01@users.noreply.github.com>
2024-02-04 22:48:26 +0530

    Update Expenses.js

--- React/src/components/expenses/Expenses.js
+++ React/src/components/expenses/Expenses.js
@@ -1,1 +1,26 @@
 
+import Card from "../UI/Card";
+import ExpenseItem from "./ExpenseItem";
+import './Expenses.css'
+
+function Expenses(props)
+{
+   
+      
+  return (
+    <Card className="expenses">
+         {
+           props.items.map((expense)=>
+            <ExpenseItem 
+              title={expense.title}
+              amount={expense.amount}
+             date={expense.date}
+             location={expense.location}
+            />
+          )}
+        
+    </Card>
+  );
+}
+
+export default Expenses
