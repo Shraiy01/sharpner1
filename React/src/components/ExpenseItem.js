5ca5f75be171d8d8ebfc5cc1920c187fab5feaa9
Shraiy01 <113162654+Shraiy01@users.noreply.github.com>
2024-02-04 15:37:24 +0530

    Update ExpenseItem.js

--- React/src/components/ExpenseItem.js
+++ React/src/components/ExpenseItem.js
@@ -1,1 +1,16 @@
 
+function ExpenseItem()
+{
+   return(
+    <div>
+    <h1>Expense Items</h1>
+     <h2>Food rs 100</h2>
+     <h2>Food rs 200</h2>
+     <h2>Food rs 300</h2>
+
+    </div>
+    
+   )
+}
+
+export default ExpenseItem
