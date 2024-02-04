e5239d26b3f8836ec53a40a55d8a7514a80b70ce
Shraiy01 <113162654+Shraiy01@users.noreply.github.com>
2024-02-04 18:05:28 +0530

    Add files via upload

---
+++ React/src/components/Expenses.js
@@ -0,0 +1,59 @@
+
+import ExpenseItem from "./ExpenseItem";
+function Expenses()
+{
+    const expenses = [
+        {
+          id: 'e1',
+          title: 'Toilet Paper',
+          amount: 94.12,
+          date: new Date(2020, 7, 14),
+          location:'Delhi',
+        },
+    
+        { 
+          
+          id: 'e2', title: 'New TV',
+         amount: 799.49, 
+         date: new Date(2021, 2, 12) ,
+         location:'Noida',
+    
+        },
+    
+        {
+          id: 'e3',
+          title: 'Car Insurance',
+          amount: 294.67,
+          date: new Date(2021, 2, 28),
+          location:'Gurgoan',
+        },
+        {
+          id: 'e4',
+          title: 'New Desk (Wooden)',
+          amount: 450,
+          date: new Date(2021, 5, 12),
+          location:'New Delhi',
+        },
+      ];
+
+
+      
+  return (
+    <div className="expenses">
+         <h1>Let's Get Started</h1>
+         
+         {
+           expenses.map((expense)=>
+            <ExpenseItem
+              title={expense.title}
+              amount={expense.amount}
+             date={expense.date}
+             location={expense.location}
+            />
+          )}
+        
+    </div>
+  );
+}
+
+export default Expenses
